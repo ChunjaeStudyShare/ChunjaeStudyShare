41e1f59daad485db780e2579d96f937b954e8608
qoiol <hjjj1324@gmail.com>
2024-12-06 17:46:50 +0900

    chat

--- src/main/java/net/fullstack7/studyShare/chat/SendChatContoller.java
+++ src/main/java/net/fullstack7/studyShare/chat/SendChatContoller.java
@@ -1,5 +1,6 @@
 package net.fullstack7.studyShare.chat;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import net.fullstack7.studyShare.domain.ChatMessage;
@@ -7,16 +8,16 @@ import net.fullstack7.studyShare.service.chat.ChatService;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class SendChatContoller {
     private final ChatService chatService;
 
     @MessageMapping("/{id}")
     @SendTo("/room/{id}")
-    public ChatMessage messageContent(@DestinationVariable int id, MessageContent messageContent, HttpSession session) throws InterruptedException {
+    public ChatMessage messageContent(@DestinationVariable int id, MessageContent messageContent) throws InterruptedException {
         ChatMessage chatMessage = chatService.addMessageToChatRoom(id, messageContent);
         if(chatMessage != null && chatMessage.getId() > 0) {
             return chatMessage;
